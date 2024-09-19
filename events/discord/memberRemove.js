9b48eb90e5b62a18b36be95b75bbbebc6f35e287
ch4rliedev <plaxv17@gmail.com>
2024-09-19 01:59:43 -0300

    melhorado a mensagem de saída de membros

--- events/discord/memberRemove.js
+++ events/discord/memberRemove.js
@@ -7,7 +7,7 @@ export async function execute(member) {
     try {
         const channel = client.channels.cache.get('1164407564096770048');
 
-        channel.send({ content: `<@${member.displayName}> nos deixou...<a:run_naruto:1260394659650342913>\nmas que Shibai Ootsutsuki o acompanhe!` });
+        channel.send({ content: `**${member.displayName}** foi embora...<a:run_naruto:1260394659650342913>\nShibai Ootsutsuki o acompanhe!\n\n||<@&1164693494544224336>||` });
 
         await userDB.updateOne({ "id_dc": member.id }, 
         {
